ae18dbacfd08c233720042d720e7436fb2ec2783
Stan <stanley.luo@bappo.com>
2019-08-07 11:54:05 +1000

    Kill server process after tests

--- e2e/inter-device-detox/parallel_commands.sh
+++ e2e/inter-device-detox/parallel_commands.sh
@@ -2,6 +2,7 @@
 
 # https://stackoverflow.com/questions/10909685/run-parallel-multiple-commands-at-once-in-the-same-terminal
 # by Alessandro Pezzato
+# Modified by Stanley Luo
 
 for cmd in "$@"; do {
   echo "Process \"$cmd\" started";
@@ -11,9 +12,10 @@ for cmd in "$@"; do {
 
 trap "kill $PID_LIST" SIGINT
 
-echo "Parallel processes have started $PID_LIST";
+echo "Parallel processes have started$PID_LIST";
 
 wait $PID_LIST
 
 echo
 echo "All processes have completed";
+kill -9 8335
